24ed4a5b670a2e178f5ea94064680c3c6e4a9f9d
ibrahimdagga <ibrahimbagga09@gmail.com>
2021-09-16 15:49:22 +0300

    upload Project3

--- app/src/main/java/com/example/picturesstories/PicturesGroup.java
+++ app/src/main/java/com/example/picturesstories/PicturesGroup.java
@@ -28,7 +28,6 @@ import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.StorageTask;
-import com.thekhaeng.recyclerviewmargin.LayoutMarginDecoration;
 
 import java.util.ArrayList;
 import java.util.List;
